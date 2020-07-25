c85f12f40912132f56e783c60cac9839d5ff291f
holsety_s <muspuzic@gmail.com>
2020-07-25 15:33:16 +0200

    sprites render based on faced direction, or idle if standing still

--- js/render.js
+++ js/render.js
@@ -36,7 +36,8 @@ function render_debug(game_debug, ctx){
         // for when autonomous movement is added
         // This works if moveToPoint() sets body angle
         //ctx.fillText('angle:'+((180*bod.angle/Math.PI)+180).toFixed(2), bod.bounds.min.x-10, bod.bounds.min.y+60);
-        //ctx.fillText('speed:'+bod.speed.toFixed(2), bod.bounds.min.x-10, bod.bounds.min.y+72);
+        ctx.fillText('angle:'+Math.floor(((180*bod.angle/Math.PI)+180)/45), bod.bounds.min.x-10, bod.bounds.min.y+60);
+        ctx.fillText('speed:'+bod.speed.toFixed(2), bod.bounds.min.x-10, bod.bounds.min.y+72);
 
         ctx.beginPath();
         ctx.arc(bod.custom.startPoint.x, bod.custom.startPoint.y, bod.custom.maxMove+(GRID_SIZE*0.5)+2, 0, Math.PI * 2, true);
@@ -98,13 +99,37 @@ function draw_Graphics(ctx, a, mode){
       var iy = i.bounds.min.y;
       var ixs = i.custom.graphics.sprite_dim.x;
       var iys = i.custom.graphics.sprite_dim.y;
-      var sx = i.custom.graphics.sheet_idle[0].x;
-      var sy = i.custom.graphics.sheet_idle[0].y;
       var dx = i.bounds.max.x - i.bounds.min.x;
       var dy = i.bounds.max.y - i.bounds.min.y;
-      // angle, angularSpeed, angularVelocity
-      // TODO: draw the proper image based on direction of body
-      // also dx and dy fuck up on rotation because the diagonal is wider, so either hardcode 32 or include a unit_dim property
+
+      //which direction? as the clock goes, 1 2, 3 4, 5 6, 7 0
+      var bod_angle = Math.floor(((180*i.angle/Math.PI)+180)/45);
+      switch (bod_angle) {
+        case 1:
+        case 2:
+          var sx = i.custom.graphics.sheet_up[0].x;
+          var sy = i.custom.graphics.sheet_up[0].y;
+          break;
+        case 3:
+        case 4:
+          var sx = i.custom.graphics.sheet_right[0].x;
+          var sy = i.custom.graphics.sheet_right[0].y;
+          break;
+        case 7:
+        case 0:
+          var sx = i.custom.graphics.sheet_left[0].x;
+          var sy = i.custom.graphics.sheet_left[0].y;
+          break;
+        default: // 5, 6
+          var sx = i.custom.graphics.sheet_idle[0].x;
+          var sy = i.custom.graphics.sheet_idle[0].y;
+          break;
+      }
+      if( !Math.floor(i.speed) ){
+        var sx = i.custom.graphics.sheet_idle[0].x;
+        var sy = i.custom.graphics.sheet_idle[0].y;
+      }
+
       ctx.drawImage(img, sx, sy, ixs, iys, ix, iy, dx, dy);
     }else{
       var ix = i.position.x - (i.custom.graphics.sprite_dim.x/2);
