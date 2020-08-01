3e5470804912803b596770b99d5eece08b411ca0
holsety_s <muspuzic@gmail.com>
2020-08-01 13:49:41 +0200

    add building type (undifferentiated from allies for now)

---
+++ js/buildings.js
@@ -0,0 +1,33 @@
+var test_turret = Bodies.rectangle(reWi-(GRID_SIZE*7), reHi-(GRID_SIZE*10), 16, 16, {
+    label: 'ally',
+    frictionAir: 1,
+    collisionFilter: {
+      category: draggable_false
+    },
+    custom: {
+      faction: 'ally',
+      turret: {
+        range: 10,
+        aType: 'direct'
+      },
+      baseMove: GRID_SIZE*5,
+      maxMove: GRID_SIZE*5,
+      startPoint: { 
+        x: reWi-(GRID_SIZE*4),
+        y: reHi-(GRID_SIZE*12)
+      },
+      graphics: {
+        sheet: true,
+        sprite: './assets/origin.png',
+        sprite_dim: {
+            x: 16,
+            y: 16
+        },
+        sheet_idle: getSprites('turret_basic', 'idle'),
+        sheet_left: getSprites('turret_basic', 'idle'),
+        sheet_right: getSprites('turret_basic', 'idle'),
+        sheet_up: getSprites('turret_basic', 'idle')
+      }
+    }
+  });
+  World.add(world, test_turret);
