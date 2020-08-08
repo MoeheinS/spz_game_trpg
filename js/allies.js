7f0cb92d003905994ac6137a4124f9c9552a916d
MoeheinS <muspuzic@gmail.com>
2020-08-08 13:55:12 +0200

    make allied units uniform, groundwork for future abilities

--- js/allies.js
+++ js/allies.js
@@ -1,76 +1,14 @@
-// 8 radius; 16 w/h, then render a 32x32 sprite with the physics body at the foot
-var test_allyGB = Bodies.circle(reWi-(GRID_SIZE*5), 300, 8, {
-//var test_allyGB = Bodies.rectangle(reWi-(GRID_SIZE*5), 300, 16, 16, {
-  label: 'ally',
-  frictionAir: 1,
-  mass: 2,
-  collisionFilter: {
-    category: draggable_false
-  },
-  custom: {
-    baseMove: GRID_SIZE*5,
-    maxMove: GRID_SIZE*5,
-    startPoint: { 
-      x: reWi-(GRID_SIZE*5),
-      y: 300
-    },
-    shape: 'circle',
-    graphics: {
-      sheet: true,
-      sprite: './assets/origin.png',
-      sprite_dim: {
-        x: 16,
-        y: 16
-      },
-      sheet_idle: getSprites('warrior_human', 'idle'),
-      sheet_left: getSprites('warrior_human', 'left'),
-      sheet_right: getSprites('warrior_human', 'right'),
-      sheet_up: getSprites('warrior_human', 'up')
-    }
-  }
-}, 10);
-//});
-World.add(world, test_allyGB);
-
-var test_allyGB2 = Bodies.circle(reWi-(GRID_SIZE*5), 600, 8, {
-  label: 'ally',
-  frictionAir: 1,
-  mass: 2,
-  collisionFilter: {
-    category: draggable_false
-  },
-  custom: {
-    baseMove: GRID_SIZE*5,
-    maxMove: GRID_SIZE*5,
-    startPoint: { 
-      x: reWi-(GRID_SIZE*5),
-      y: 300
-    },
-    shape: 'circle',
-    graphics: {
-      sheet: true,
-      sprite: './assets/origin.png',
-      sprite_dim: {
-        x: 16,
-        y: 16
-      },
-      sheet_idle: getSprites('ratty', 'idle'),
-      sheet_left: getSprites('ratty', 'left'),
-      sheet_right: getSprites('ratty', 'right'),
-      sheet_up: getSprites('ratty', 'up')
-    }
-  }
-}, 10);
-World.add(world, test_allyGB2);
-
-
-
 class UnitEnt {
   constructor(
       spawnCoord,
-      faction
+      faction,
+      spriteName,
+      hp,
+      attackCD,
+      moveSpeed,
+      damage // all these things should be taken from a unit index, but for now...
   ) {
-      this.body = Bodies.circle(spawnCoord.x, spawnCoord.y, 8, {
+      this.body = Bodies.circle(spawnCoord.x+(GRID_SIZE*0.5), spawnCoord.y+(GRID_SIZE*0.75), 8, {
         label: 'ally',
         frictionAir: 1, // magic numbers
         mass: 2,        // magic numbers
@@ -80,6 +18,10 @@ class UnitEnt {
         custom: {
           faction: faction,
           shape: 'circle',
+          hp_max: hp,
+          hp_current: hp,
+          state: 'idle',
+
           graphics: {
             sheet: true,
             sprite: './assets/origin.png',
@@ -87,39 +29,32 @@ class UnitEnt {
               x: 16,
               y: 16
             },
-            sheet_idle: getSprites('ratty', 'idle'),
-            sheet_left: getSprites('ratty', 'left'),
-            sheet_right: getSprites('ratty', 'right'),
-            sheet_up: getSprites('ratty', 'up')
+            sheet_idle: getSprites(spriteName, 'idle'),
+            sheet_left: getSprites(spriteName, 'left'),
+            sheet_right: getSprites(spriteName, 'right'),
+            sheet_up: getSprites(spriteName, 'up')
           }
         }
       }, 10);
+      World.add(world, this.body);
+      console.log(this.body.position);
+      return this.body;
   }
-  advance() { // advance the projectile
-      if( this.lifetime >= this.lifetimeMax ){
-          // mostly STUB. apply pain? stop rendering this? remove this from something?
-          // TODO: if there's an array, remove self from said array
-          projectiles_Array.splice(projectiles_Array.indexOf(this), 1);
-          this.applyPain();
-      }else{
-          this.lifetime++;
-          let progress = 1 - ( this.lifetime / this.lifetimeMax );
-          // console.warn('goal');
-          // console.table(this.goal);
-          // console.warn('origin');
-          // console.table(this.origin);
-
-          let diff_x = Math.abs( this.origin.x - this.goal.x ) * progress;
-          let diff_y = Math.abs( this.origin.y - this.goal.y ) * progress;
-
-          this.position.x = ( this.goal.x > this.origin.x ? this.goal.x - diff_x : this.goal.x + diff_x );
-          this.position.y = ( this.goal.y > this.origin.y ? this.goal.y - diff_y : this.goal.y + diff_y );
-          // TODO: this might be worthwhile, but the ends are too big?
-          //this.arcHeight = Math.sin( Math.PI * ( progress - 0.00000001 ) );
-      }
+  advance() { 
+    // STUB
+    /*
+      acquire target, move to target
+    */
   }
   applyPain() {
-      //STUB
-      console.log('ow!');
+    //STUB
+    console.log('ow!');
+  }
+  die() {
+    // STUB
+    // spawn a little tombstone doodad
   }
 }
+
+var test_allyGB2 = new UnitEnt( new Coordinate( GRID_SIZE*8, GRID_SIZE*2 ), 'friendlies', 'ratty', 100 );
+new UnitEnt( new Coordinate( GRID_SIZE*20, GRID_SIZE*2 ), 'friendlies', 'warrior_human', 100 );
